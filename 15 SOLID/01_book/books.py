6e06d633b1757cec9b614a9853878f462cb778fb
Petar Radoslavov Petrov <prpetrovv@gmail.com>
2024-01-16 23:58:23 +0200

    Add: 15 SOLID - books.py

---
+++ 15 SOLID/01_book/books.py
@@ -0,0 +1,26 @@
+from typing import List, Optional
+
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.page = 0
+
+    def turn_page(self, page):
+        self.page = page
+
+
+class Library:
+    def __init__(self):
+        self.books: List[Book] = []
+
+    def add_book(self, book: Book):
+        self.books.append(book)
+
+    def find_book(self, title: str) -> Optional[Book]:
+        try:
+            book = [b for b in self.books if b.title == title][0]
+            return book
+        except IndexError:
+            return None
